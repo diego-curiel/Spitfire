4896211531a9d1ef23e410e13758088a00ec376a
diego-curiel <177177256+diego-curiel@users.noreply.github.com>
2025-02-27 15:25:54 -0400

    add option to change the output file directory and replace the recursive splitting with a do-while loop

--- src/app.py
+++ src/app.py
@@ -5,7 +5,6 @@ import cProfile as pr
 import os
 from pathlib import Path
 from typing import Any
-import sys
 
 EXCEL_MAX_ROW = 1048576
 
@@ -53,6 +52,8 @@ def generate_arguments() -> ap.Namespace:
                         including the file header.
                         """,
                         action="store_true", default=False)
+    parser.add_argument("-o", "--output", help="Path to the output file.",
+                        required=True, type=Path)
 
     return parser.parse_args()
 
@@ -87,9 +88,9 @@ def file_exist(file_path: Path, nonexistent_msg:str|None=None,
     if not file_path.exists(follow_symlinks=True):
         raise SystemExit(nonexistent_msg or dft_nonexistent_msg)
     
-    dft_no_access_msg = "You do not have permissions to read {f}".format(
-            f=file_path
-        )
+    dft_no_access_msg = "You do not have permissions to read {f}."
+
+    dft_no_access_msg = dft_no_access_msg.format(f=file_path)
 
     # Check if the file is readable
     if not os.access(file_path, os.R_OK):
@@ -112,8 +113,11 @@ def get_file_name(file_path:Path|str) -> str:
     """
     if not isinstance(file_path, Path):
         file_path = Path(file_path)
+    filename = file_path.name
+    filename, extension = os.path.splitext(filename)
+    del extension
 
-    return ".".join(file_path.name.split(".")[:-1])
+    return filename
 
 
 def split_dataset(df:pd.DataFrame, 
@@ -143,7 +147,7 @@ def split_dataset(df:pd.DataFrame,
             )
     
     # Gather all categories available on this dataframe
-    categories = set(df[category_column])
+    categories = set(df.loc[:,category_column])
 
     # Filter the chunk by the values in the group_column
     groups = dict()
@@ -202,94 +206,93 @@ def grouped_dataframes_generator(input_path:Path|str,
         yield value
 
 
-def handle_sheet_overflow(df:pd.DataFrame, sheet_name:str,
-                    max_row:int, index:int=1, 
-                    result:list|None=None) -> list:
-    """
-    Description
-    -----------
-    This function takes a DataFrame and counts its rows. If the DataFrame rows 
-    are less or equal than the max_row value then it returns a dictionary with 
-    the sheet name as the key, and the DataFrame as value. Otherwise, if the 
-    DataFrame row count is bigger than the max_row value, then the function will
-    recursively split the DataFrame by the max_row value, adding a numeral to
-    the sheet name. E.G:
-        max_row = 1024
-        Sheet_A = 2100
+def handle_sheet_overflow(df:pd.DataFrame, sheet_name:str, 
+                          max_row:int) -> list[tuple]:
+    """Description:
+    This function splits a DataFrame into chunks based on the amount of rows
+    passed on the max_row parameter, then it returns a list that contains a
+    tuple for each chunk, whose first value is the new name of the sheet.
+    E.G:
+    max_row = 1024
+    Sheet_A = 2100
 
-        Sheet_A = 1024
-        Sheet_A_2 = 1024
-        Sheet_A_3 = 52
-    ---
-    Arguments
-    ---------
-    df:pd.DataFrame = Dataframe to split.
-    sheet_name: Name of the sheet to be saved.
-    max_row: Max amount of rows supported by the filetype.
-    index:int = Iteration index number.
-    result:list|None (default=None) = Result of the past iteration.
-    ---
-    Return
-    ------
-    list = list containing a tuple with the Sheet names and the DataFrames.
+    Sheet_A = 1024
+    Sheet_A_2 = 1024
+    Sheet_A_3 = 52
+
+    Arguments:
+    df (pd.DataFrame) - Dataframe to split.
+    sheet_name (str) - Name of the sheet to be saved.
+    max_row (int) - Max amount of rows supported by the filetype.
+
+    Returns:
+    list[tuple] - list containing a tuple with the Sheet names and the DataFrames.
     """
-    # Create an empty list to save results
-    if not isinstance(result, list):
-        result = list()
-    # Stop when there's no splitting left to do
-    if len(df) == 0:
-        return result
-    # Create a new sheet_name
-    new_name = sheet_name
-    if index > 1:
-        new_name = "{sn}_{i}".format(sn=sheet_name, i=index)
-    # Append the split DataFrame to the result list
-    result.append((new_name, df.iloc[:max_row]))
-    # Search again
-    return handle_sheet_overflow(df=df.iloc[max_row:], sheet_name=sheet_name,
-                           max_row=max_row, index=index + 1,
-                           result=result)
+    # Initialize the variables
+    result = list()
+    index = 1
+    new_df = df.copy()
+    new_name = sheet_name 
+    #Do while
+    while True:
+        # Stop when there's no splitting left to do
+        if len(new_df) == 0:
+            break
+
+        # Create a new sheet_name
+        if index > 1:
+            new_name = "{sn}_{i}".format(sn=sheet_name, i=index)
+
+        # Append the split DataFrame to the result list
+        sheet_tuple:tuple[str, pd.DataFrame] = (new_name, new_df.iloc[:max_row])
+        new_df:pd.DataFrame = new_df.iloc[max_row:]
+        result.append(sheet_tuple)
+        # Increase the index
+        index += 1
+    
+    return result
 
 
 def main():
-    sys_args = generate_arguments()
+    SYS_ARGS = generate_arguments()
 
-    file_path = Path(sys_args.input)
+    INPUT_PATH:Path = SYS_ARGS.input
     
     # Check file accessibility
-    file_exist(file_path=file_path)
-
-    parent_directory = file_path.parent
-
-    file_name = get_file_name(file_path)
+    file_exist(file_path=INPUT_PATH)
     
-    OUTPUT_FILE = parent_directory.joinpath(
-            "{f}.split.xlsx".format(f=file_name)
-        )
-    RECURSION_DEPTH = sys.getrecursionlimit()
+    INPUT_DIR = INPUT_PATH.parent
+
+    OUTPUT_FILE:Path = SYS_ARGS.output
+    
+    MAX_ROW = SYS_ARGS.max_row
+
+    # If the save file does not exist, raise an exception
+    if not OUTPUT_FILE.parent.exists():
+        raise SystemExit("The save directory does not exist!")
 
     # Handle bad max_row inputs
-    if sys_args.max_row > EXCEL_MAX_ROW:
-        sys_err = "The maximum supported range for rows is: {r}".format(
-                r=EXCEL_MAX_ROW
-            )
+    if SYS_ARGS.max_row > EXCEL_MAX_ROW:
+        sys_err = "The maximum supported range for rows is: {r}"
+        sys_err = sys_err.format(r=EXCEL_MAX_ROW)
         raise SystemExit(sys_err)
+
     # Set profiler to create the performance file
     with pr.Profile() as profiler:
         # Yield the grouped DataFrames
         grouped_dataframes = grouped_dataframes_generator(
-                input_path=file_path,
-                sys_args=sys_args
+                input_path=INPUT_PATH,
+                sys_args=SYS_ARGS
             )
 
-        if sys_args.uppercase:
+        if SYS_ARGS.uppercase:
             print("The files will be saved with textfields in uppercase")
             input("Press any key to continue...")
 
         # Save the groups into Excel File
-        with pd.ExcelWriter(
-                OUTPUT_FILE, mode="w", engine="openpyxl") as writer:
-            print("Reading file...")
+        print("Reading file...")
+        with pd.ExcelWriter(OUTPUT_FILE, mode="w", 
+                            engine="openpyxl") as writer:
             for group in grouped_dataframes:
                 category, df = group
                 df:pd.DataFrame
@@ -299,7 +302,7 @@ def main():
                     category = str(category)
 
                 # If needed, set all of the dataset text fields to uppercase
-                if sys_args.uppercase:
+                if SYS_ARGS.uppercase:
                     to_upper = lambda x: str(x).upper() if isinstance(x, str) else x
                     df = df.map(to_upper)
                     df.columns = [to_upper(x) for x in df.columns]
@@ -308,26 +311,14 @@ def main():
                 print("saving group {c}...".format(c=category))
                 print("group size: {s}".format(s=len(df)))
                 
-                # Fix to handle stack overflow
-                max_row = sys_args.max_row
-                splits = len(df) // max_row
-
-                if RECURSION_DEPTH < splits:
-                    # minus two iterations
-                    sys_err = "ERROR!, the row count for {c} is too low!"
-                    sys_err_ex = " Try increasing the --max-row option value."
-                    sys_err = sys_err.format(c=category) + sys_err_ex
-                    raise SystemExit(sys_err)
-
                 df_split = handle_sheet_overflow(df=df, sheet_name=category,
-                                                 max_row=max_row)
-
+                                                 max_row=MAX_ROW)
                 for sheet_name, split_sheet in df_split:
                     split_sheet.to_excel(writer, index=False, 
                                          sheet_name=sheet_name)
 
         # Gather performance information with cProfile
-        performance_info = parent_directory.joinpath("profiler_report")
+        performance_info = INPUT_DIR.joinpath("profiler_report")
         profiler.create_stats()
         profiler.dump_stats(performance_info)
     
