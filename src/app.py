c921aeff3587508b8a339235f21ad8d1f02091df
diego-curiel <177177256+diego-curiel@users.noreply.github.com>
2025-05-25 11:14:30 -0400

    code refactor

--- src/app.py
+++ src/app.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+
 import argparse as ap
 import cProfile as pr
+import math
 import os
 from pathlib import Path
-from typing import Any
+from typing import Union
 
 import pandas as pd
 
@@ -10,13 +13,10 @@ EXCEL_MAX_ROW = 1048576
 
 def generate_arguments() -> ap.Namespace:
     """
-    Description
-    -----------
     Generate the system arguments used in this script.
-    --------
+    
     Return
-    --------
-    ap.Namespace = Object that contains all arguments parsed.
+        ap.Namespace: Object that contains all arguments parsed.
     """
     parser = ap.ArgumentParser(
             prog="Spitfire",
@@ -52,8 +52,14 @@ def generate_arguments() -> ap.Namespace:
                         including the file header.
                         """,
                         action="store_true", default=False)
+
+    parser.add_argument("-p", "--prefix",
+                        help="Add a prefix to the name of the output file/s.",
+                        default="", type=str)
+
     parser.add_argument("-o", "--output", help="Path to the output file.",
                         required=True, type=Path)
+
     parser.add_argument("--separate", 
                         help="Save the groups into separated files each.",
                         action="store_true")
@@ -61,28 +67,23 @@ def generate_arguments() -> ap.Namespace:
     return parser.parse_args()
 
 
-def file_exist(file_path: Path, nonexistent_msg:str|None=None, 
-               no_access_msg:str|None=None) -> None:
+def do_file_exist(file_path: Path, nonexistent_msg:str|None=None, 
+               no_access_msg:str|None=None) -> bool:
     """
-    Description
-    -----------
     Function to check if the filepath exist and it is available to be read.
-    ------
+
     Params
-    ------
-    file_location:Path = The location to be asserted as accessible.
-    nonexistent_msg:str = The message to be shown when the path doesn't exist.
-    no_access_msg:str = The message to be shown when the file exist but it is
-    not readable.
-    -----
+        file_location (Path): The location to be asserted as accessible.
+        nonexistent_msg (str): The message to be shown when the path doesn't exist.
+        no_access_msg (str): The message to be shown when the file exist but it is
+        not readable.
+
     Raise
-    -----
-    SystemExit = If the file does not exist.
-    SystemExit = If the file exist but is not accessible (read mode).
-    ------
+        SystemExit = If the file does not exist.
+        SystemExit = If the file exist but is not accessible (read mode).
+
     Return
-    ------
-    None
+        bool: True if both conditions passed.
     """
 
     dft_nonexistent_msg = "{f} does not exist!".format(f=file_path)
@@ -98,21 +99,19 @@ def file_exist(file_path: Path, nonexistent_msg:str|None=None,
     # Check if the file is readable
     if not os.access(file_path, os.R_OK):
         raise SystemExit(no_access_msg or dft_no_access_msg)
+    
+    return True
 
 
 def get_file_name(file_path:Path|str) -> str:
     """
-    Description
-    -----------
     Get the name of the file without the extension.
-    -----
-    Args
-    -----
-    file_path:Path|str = path to the file location.
-    ------
-    Return
-    ------
-    str = file name without extension.
+
+    Arguments:
+        file_path (Path|str): Path to the file location.
+
+    Returns:
+        str: File name without extension.
     """
     if not isinstance(file_path, Path):
         file_path = Path(file_path)
@@ -123,15 +122,15 @@ def get_file_name(file_path:Path|str) -> str:
     return filename
 
 
-def dataframe_upercase(df:pd.DataFrame)->pd.DataFrame:
-    """Description:
+def uppercase_dataframe(df:pd.DataFrame)->pd.DataFrame:
+    """
     Takes a DataFrame and sets all of its textfields to uppercase.
 
     Parameters:
-    df (pd.DataFrame) - The DataFrame whose textfields will be set to upper.
+        df (pd.DataFrame): The DataFrame whose textfields will be set to upper.
 
     Returns:
-    pd.Dataframe - A copy of the passed DataFrame with each textfield set to upper.
+        pd.Dataframe: A copy of the passed DataFrame with each textfield set to upper.
     """
     textfield_to_upper = lambda x: str(x).upper() if isinstance(x, str) else x
 
@@ -141,239 +140,242 @@ def dataframe_upercase(df:pd.DataFrame)->pd.DataFrame:
     return df
 
 
-def split_dataset(df:pd.DataFrame, 
-                  category_column:str|int) -> dict[Any, pd.DataFrame]: 
+def split_dataset(
+    df:pd.DataFrame, 
+    category_column:str|int
+) -> dict[Union[str, int], pd.DataFrame]: 
     """
-    Description
-    ------------
     Splits the dataset into multiple DataFrames based on unique category values.
+
     Each resulting Dataframe contains only the rows corresponding to a single
     category, preserving the original order of the data. 
-    ----
-    Args
-    ----
-    df:pd.DataFrame = Input dataset.
-    category_column:str|int = The horizontal index where the groups are located.
-    ------
+
+    Arguments:
+        df: (pd.DataFrame): The DataFrame to be separated by its categories.
+        category_column (str|int): The name of the category column.
+
     Return
-    -------
-    dict = Hashmap that contains the groups of DataFrames.
+        dict: Hashmap that contains the groups of DataFrames.
     """
     # Check if the category_column exist in the Input dataset
     if not category_column in df.columns:
         raise SystemExit(
-                "The column {c} does not exist in the input file!".format(
-                        c=category_column
-                    )
-            )
-    
+            f"The column {category_column} does not exist in the input file!"
+        )
+
     # Gather all categories available on this dataframe
-    categories = set(df.loc[:,category_column])
+    category_set = set(df.loc[:,category_column])
 
     # Filter the chunk by the values in the group_column
     groups = dict()
-    for category in categories:
+    for category in category_set:
         groups[category] = df[df[category_column] == category]
-            
+
     return groups
 
 
-def grouped_dataframes_generator(input_path:Path|str, 
-                                sys_args:ap.Namespace):
+def split_csv_file(
+    input_path:Path|str, 
+    category_column: Union[int, str],
+) -> dict[Union[str, int], pd.DataFrame]:
     """
-    Description
-    ---
-    This function takes a Dataset and applies split_dataset on it while saving
-    memory (Lazy loading). All of the groups are then yielded as a tuple that 
-    contains the category value and the DataFrame itself.
-    ---
-    Args
-    ---
-    input_path:Path|str = Path to the dataset to be processed.
-    sys_args:ap.Namespace = System Arguments.
-    ---
-    Yields
-    ---
-    tuple[str, pd.DataFrame] = (category, DataFrame)
+    Reads the file from the given input_path by chunks, then calls the
+    split_dataset function for each chunk.
+       
+    Arguments:
+        input_path (Path | str): Path to the dataset to be processed.
+        category_column (int | str): Name of the column containing the categories.
+       
+    Returns:
+        dict[str | int, pd.DataFrame]: Dictionary containing the groups.
     """
     if not isinstance(input_path, Path):
         input_path = Path(input_path)
 
-
-    ultimate_groups = dict()
+    grouped_dataframes: dict[Union[str, int], pd.DataFrame] = dict()
     with pd.read_csv(input_path, chunksize=100000) as file:
-        # List of files to turn into Excel later
-        # TODO: Add options for files longer than the max row count of Excel
-
-        # Chunk processing loop
         for chunk in file:
-            print(".  ", end="\r")
             chunk:pd.DataFrame
-            temporary_groups = split_dataset(df=chunk, 
-                                      category_column=sys_args.category_column)
+
+            # Loading animation
+            print(".  ", end="\r")
             print(".. ", end="\r")
-            
+            print("...", end="\r")
+            # ==================
+
+            temporary_groups = split_dataset(
+                df=chunk, 
+                category_column=category_column
+            )
+
             for category in temporary_groups.keys():
-                ult_group = ultimate_groups.get(category, pd.DataFrame())
+                group_df = grouped_dataframes.get(category, pd.DataFrame())
                 temp_group = temporary_groups.get(category, pd.DataFrame())
                 # Remove unwanted characters from the category such as '/'
-                category = category.replace('/', '')
-                ultimate_groups[category] = pd.concat([ult_group, temp_group],
-                                                      ignore_index=True)
+                category = str(category).replace('/', '')
+                grouped_dataframes[category] = pd.concat(
+                    [group_df, temp_group],
+                    ignore_index=True
+                )
 
-            print("...", end="\r")
+        # End of the loading animation
         print("DONE...", end="\n")
+        # ==================
 
-    for value in tuple(ultimate_groups.items()):
-        yield value
+    return grouped_dataframes
+
+
+def num_digits(n: int) -> int:
+    """
+    Get the amount of digits in a positive integer number.
+
+    Parameters:
+        n (int): the number whose amount of digits we want to know.
+
+    Returns:
+        int: The amount of digits in the given number.
+
+    Raises:
+        ValueError: If the number passed is negative.
+    """
+    if n < 0:
+        raise ValueError("You cannot pass negative numbers")
+
+    if n == 0:
+        return 1
+
+    return math.floor(math.log10(n)) + 1
 
 
 def handle_sheet_overflow(df:pd.DataFrame, sheet_name:str, 
-                          max_row:int) -> list[tuple]:
+                          max_row:int) -> list[tuple[str | int, pd.DataFrame]]:
     """
-    Description:
-    This function splits a DataFrame into chunks based on the amount of rows
+    Splits a DataFrame into chunks based on the amount of rows
     passed on the max_row parameter, then it returns a list that contains a
     tuple for each chunk, whose first value is the new name of the sheet.
-    E.G:
-    max_row = 1024
-    Sheet_A = 2100
 
-    Sheet_A = 1024
-    Sheet_A_2 = 1024
-    Sheet_A_3 = 52
+    E.G:
+        max_row = 1024
+        Sheet_A = 2100
+
+        Sheet_A = 1024
+        Sheet_A_2 = 1024
+        Sheet_A_3 = 52
 
     Arguments:
-    df (pd.DataFrame) - Dataframe to split.
-    sheet_name (str) - Name of the sheet to be saved.
-    max_row (int) - Max amount of rows supported by the filetype.
+        df (pd.DataFrame): Dataframe to split.
+        sheet_name (str): Name of the sheet to be saved.
+        max_row (int): Max amount of rows supported by the filetype.
+
 
     Returns:
-    list[tuple] - list containing a tuple with the Sheet names and the DataFrames.
+        list[tuple]: List containing a tuple with the Sheet names and the DataFrames.
     """
-    # Initialize the variables
     result = list()
-    index = 1
-    new_df = df.copy()
-    new_name = sheet_name 
-    #Do while
-    while True:
-        # Stop when there's no splitting left to do
-        if len(new_df) == 0:
-            break
+    total_rows = len(df)
+    num_chunks = math.ceil(total_rows / max_row)
+    pad_width = num_digits(num_chunks)
 
-        # Create a new sheet_name
-        if index > 1:
-            new_name = "{sn}_{i}".format(sn=sheet_name, i=index)
+    for sheet_count, start in enumerate(range(0, total_rows, max_row), start=1):
+        end = start + max_row
+        # Suffix example: _001, _002
+        suffix = f"_{str(sheet_count).rjust(pad_width, "0")}"
+        # Excel has a limit of 31 characters for the sheet name
+        max_base_len = 31 - len(suffix)
+        trimmed_name = sheet_name[:max_base_len].rstrip("_ ")
+        final_name = f"{trimmed_name}{suffix}"
+        result.append((final_name, df.iloc[start:end]))
 
-        # Append the split DataFrame to the result list
-        sheet_tuple:tuple[str, pd.DataFrame] = (new_name, new_df.iloc[:max_row])
-        new_df:pd.DataFrame = new_df.iloc[max_row:]
-        result.append(sheet_tuple)
-        # Increase the index
-        index += 1
-    
     return result
 
 
 def main():
-    SYS_ARGS = generate_arguments()
-    
-    # Input file information
-    INPUT_PATH:Path = SYS_ARGS.input
-    INPUT_DIR = INPUT_PATH.parent
-    # Check file accessibility
-    file_exist(file_path=INPUT_PATH)
-    
-    # Output file information
-    OUTPUT_PATH:Path = SYS_ARGS.output
-    OUTPUT_DIR = OUTPUT_PATH.parent
-    OUTPUT_FILENAME = get_file_name(OUTPUT_PATH)
-   
-    # Max rows per sheet
-    MAX_ROW = SYS_ARGS.max_row
+    sys_args = generate_arguments()
+    # Check the input file accessibility
+    do_file_exist(file_path=sys_args.input)
 
-    # If the save file does not exist, raise an exception
-    if not OUTPUT_PATH.parent.exists():
+    output_file_name = get_file_name(sys_args.output)
+
+    # Validate the save directory
+    if not sys_args.output.parent.exists():
         raise SystemExit("The save directory does not exist!")
 
-    # Handle invalid max_row inputs
-    if SYS_ARGS.max_row > EXCEL_MAX_ROW:
-        sys_err = "The maximum supported range for rows is: {r}"
-        sys_err = sys_err.format(r=EXCEL_MAX_ROW)
+    # Validate the max-row argument
+    if sys_args.max_row > EXCEL_MAX_ROW:
+        sys_err = "The maximum supported range for rows is: {max_rows}"
+        sys_err = sys_err.format(max_rows=EXCEL_MAX_ROW)
         raise SystemExit(sys_err)
 
     # Set profiler to create the performance file
     with pr.Profile() as profiler:
-        # Yield the grouped DataFrames
-        grouped_dataframes = grouped_dataframes_generator(
-                input_path=INPUT_PATH,
-                sys_args=SYS_ARGS
-            )
-        
-        # Small function to set text to uppercase ignoring other types
-        to_upper = lambda x: x.upper() if isinstance(x, str) else x
+        # Save the groups into Excel File
+        print("Reading file...")
 
-        if SYS_ARGS.uppercase:
+        grouped_dataframes = split_csv_file(
+            input_path=sys_args.input,
+            category_column=sys_args.category_column,
+        )
+
+        if sys_args.uppercase:
             print("The files will be saved with textfields in uppercase")
             input("Press any key to continue...")
 
-        # Save the groups into Excel File
-        print("Reading file...")
-        # Option to save the groups into separated files each.
-        if SYS_ARGS.separate:
-            for category, df in grouped_dataframes:
-                # Check if the user wants their spreadsheet all in uppercase
-                if SYS_ARGS.uppercase:
-                    df:pd.DataFrame = dataframe_upercase(df)
-                    category = to_upper(category)
+            grouped_dataframes = {str(k).upper():uppercase_dataframe(v)
+                                  for k,v in grouped_dataframes.items()}
 
-                # Change file name according to the category
-                output_file = "{parent}/{filename}.{category}.xlsx"
-                output_file = output_file.format(parent=OUTPUT_DIR,
-                                                 filename=OUTPUT_FILENAME,
-                                                 category=category)
+        # Template for the output file path
+        output_template = "{parent}/{prefix}{category}.{suffix}.xlsx"
 
-                # Truncate the category name to comply with
-                # the sheet name limit for Excel
-                sheet_name = category[:30]
+        # Handle sheet overflow for each group
+        sheets_per_group = {
+            category:handle_sheet_overflow(
+                df=df,
+                sheet_name=str(category),
+                max_row=sys_args.max_row,
+            )
+            for category, df in grouped_dataframes.items()
+        }
 
-                with pd.ExcelWriter(output_file, mode="w") as excel_writer:
-                    sheet_tuple = handle_sheet_overflow(df=df,
-                                                        sheet_name=sheet_name,
-                                                        max_row=MAX_ROW)
-                    print_msg = "Saving group {group_name}..."
-                    print_msg = print_msg.format(group_name=category)
-                    print(print_msg)
+        # This loop is for saving each group into its corresponding Excel files
 
-                    for sheet_name, df in sheet_tuple:
-                        df.to_excel(excel_writer, index=False, 
-                                    sheet_name=sheet_name)
+        
+        if sys_args.separate:
+            # Save the groups separately
+            for category, sheet_tuples in sheets_per_group.items():
+                print(f"saving the file for the category {category}")
+                output_path = output_template.format(
+                    parent=Path(sys_args.output).parent,
+                    prefix=sys_args.prefix,
+                    category=category,
+                    suffix=output_file_name,
+                )
+                # Save the corresponding sheets to each separated file
+                with pd.ExcelWriter(output_path) as excel_writer:
+                    for sheet_name, content in sheet_tuples:
+                        content.to_excel(excel_writer, index=False,
+                                         sheet_name=str(sheet_name))
 
+        # Save the groups into one file
         else:
-            with pd.ExcelWriter(OUTPUT_PATH, mode="w") as excel_writer:
-                for category, df in grouped_dataframes:
-                    # Check if the user wants their spreadsheet all in uppercase
-                    if SYS_ARGS.uppercase:
-                        df:pd.DataFrame = dataframe_upercase(df)
-                        category = to_upper(category)
-
-                    sheet_tuple = handle_sheet_overflow(df=df,
-                                                        sheet_name=category,
-                                                        max_row=MAX_ROW)
-                    print_msg = "Saving group {group_name}..."
-                    print_msg = print_msg.format(group_name=category)
-                    print(print_msg)
-
-                    for sheet_name, df in sheet_tuple:
-                        df.to_excel(excel_writer, index=False,
-                                    sheet_name=sheet_name)
+            output_path = output_template.format(
+                parent=Path(sys_args.output).parent,
+                prefix= sys_args.prefix,
+                category=output_file_name,
+                suffix="split",
+            )
+            print(f"Saving the file {output_path}")
+            with pd.ExcelWriter(output_path) as excel_writer:
+                for _, sheet_tuples in sheets_per_group.items():
+                    for sheet_name, content in sheet_tuples:
+                        content: pd.DataFrame
+                        content.to_excel(excel_writer, index=False, 
+                                        sheet_name=str(sheet_name))
 
         # Gather performance information with cProfile
-        performance_info = INPUT_DIR.joinpath("profiler_report")
+        performance_info = Path(sys_args.output).parent.joinpath("profiler_report")
         profiler.create_stats()
         profiler.dump_stats(performance_info)
-    
+
 
 if __name__ == "__main__":
     main()
